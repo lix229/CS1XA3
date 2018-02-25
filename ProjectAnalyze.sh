bde12461bae570cb6eb6a9830a4b0b5c99555bc3
Shawn Li <lix229@mills.cas.mcmaster.ca>
2018-02-24 21:36:10 -0500

    ProjectAnalyze.sh Modified

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -21,11 +21,21 @@ echo
 
 #Checking if the course resouces page has been updated
 URL="http://www.cas.mcmaster.ca/~dalvescb/#outline-container-orgd3c9a86"
+if [ ! -f old.html ]; then
+ touch old.html
+fi
 curl $URL -L --compressed -s > new.html
 DIFF_OUTPUT="$(diff new.html old.html)"
 if [ "0" != "${#DIFF_OUTPUT}" ]; then
  printf "New recouces are now available on the course website.\n"
+ read -p 'Would you like to download them now?(Y/N)'$'\n' ans
  echo
+ case $ans in 
+  [Yy]* ) wget -r -nd -nc -A pdf -P Resources/ http://www.cas.mcmaster.ca/~dalvescb/#outline-container-orgd3c9a86;
+  echo
+  printf "New PDFs downloaded in Resources.\n";;
+  [Nn]* ) echo;;
+ esac
 else
  printf "The resources page has not been updated yet.\n"
  echo
